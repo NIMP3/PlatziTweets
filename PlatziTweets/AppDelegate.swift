980ee7533101917b3f8ff83f237041659f291862
EDWIN YOVANY <yovany.dev@gmail.com>
2022-01-19 11:20:09 -0500

    Archivos iniciales del proyecto

---
+++ PlatziTweets/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  PlatziTweets
+//
+//  Created by Centro de Informática on 17/01/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
